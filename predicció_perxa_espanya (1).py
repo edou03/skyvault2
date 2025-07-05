259efb3687daf1d78430229306c936d80dadacb8
edou03 <edouardruizj@gmail.com>
2025-07-05 23:28:47 +0200

    predicció_perxa_espanya (1).py

--- predicció_perxa_espanya (1).py
+++ predicció_perxa_espanya (1).py
@@ -2,23 +2,21 @@ import pickle
 import pandas as pd
 import streamlit as st
 
-
-
 # 📥 Carrega el model guardat
 with open("model_lr.pkl", "rb") as f:
     model = pickle.load(f)
 
 # 🧪 Exemple de dades d’entrada (modifica-ho per provar diferents atletes)
 input_data = {
-    'Genero': ,  # 1 = Home, 0 = Dona
-    'Talla': ,
-    'peso':,
-    'Velocidad 10-5m': ,
-    'Aceleracion 10-5m': ,
-    'Distancia Batida': ,
-    'grip': ,
-    'pole length': ,
-    'pole flex': 
+    'Genero':1 ,  # 1 = Home, 0 = Dona
+    'Talla': 1.78,
+    'peso':68,
+    'Velocidad 10-5m':8.3 ,
+    'Aceleracion 10-5m': 0.3,
+    'Distancia Batida': 3.5,
+    'grip': 4.4,
+    'pole length':4.6,
+    'pole flex':18.8 
 }
 
 # 🧮 Converteix a DataFrame i prediu
